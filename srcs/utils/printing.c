a26664db7b92de5220f8a2cd8127335b1fe82e22
Duarte Areias dos Reis <duarte_reis_95@hotmail.com>
2021-11-15 10:31:53 +0000

    Fixes Norm

--- srcs/utils/printing.c
+++ srcs/utils/printing.c
@@ -6,13 +6,22 @@
 /*   By: dareias- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 18:30:33 by dareias-          #+#    #+#             */
-/*   Updated: 2021/11/15 10:20:19 by dareias-         ###   ########.fr       */
+/*   Updated: 2021/11/15 10:26:59 by dareias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 long long int	get_timeval(t_timeval t_start);
+
+static void	update_killers(t_philo *philo)
+{
+	if (philo->times_eaten == philo->time.full && philo->state == eating)
+		if (*philo->all_full > 0)
+			*philo->all_full = *philo->all_full - 1;
+	if (philo->state == dead && *philo->killer == 0)
+		*philo->killer = 1;
+}
 
 void	print_state(t_philo *philo)
 {
@@ -26,11 +35,7 @@ void	print_state(t_philo *philo)
 	state[3] = "is thinking";
 	state[4] = "died";
 	t = get_timeval(philo->time.t_start);
-	if (philo->times_eaten == philo->time.full && philo->state == eating)
-		if (*philo->all_full > 0)
-			*philo->all_full = *philo->all_full - 1;
-	if (philo->state == dead && *philo->killer == 0)
-		*philo->killer = 1;
+	update_killers(philo);
 	if (*philo->killer != 2 && *philo->all_full != -2)
 	{	
 		printf("%lld %i %s\n", t / 1000, philo->id, state[philo->state]);
