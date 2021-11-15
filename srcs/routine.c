ead9c73dc6be4a420c22da4d89e21028d4f34771
Duarte Areias dos Reis <duarte_reis_95@hotmail.com>
2021-11-15 14:44:09 +0000

    Fixes norm

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: dareias- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 18:20:24 by dareias-          #+#    #+#             */
-/*   Updated: 2021/11/15 13:08:55 by dareias-         ###   ########.fr       */
+/*   Updated: 2021/11/15 14:43:54 by dareias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ static int	eat(t_philo *philo);
 static int	go_sleep(t_philo *philo);
 static int	think(t_philo *philo);
 static int	ft_full(t_philo *philo);
-
-//static void print_test(t_philo *philo);
 
 void	*routine(void *void_philo)
 {
@@ -29,7 +27,6 @@ void	*routine(void *void_philo)
 	{
 		if (philo->state == thinking && lock_forks(philo))
 			eat(philo);
-	//	print_test(philo);
 		if (philo->state == eating)
 			go_sleep(philo);
 		if (starvation(philo) < 0)
@@ -49,10 +46,7 @@ static int	eat(t_philo *philo)
 	philo->times_eaten++;
 	gettimeofday(&philo->last_meal, NULL);
 	print_state(philo);
-	//print_test(philo);
 	ft_sleep(philo->last_meal, philo->time.t_to_e);
-	//usleep(philo->time.t_to_e * 1000);
-	//print_test(philo);
 	return (1);
 }
 
@@ -85,16 +79,6 @@ static int	think(t_philo *philo)
 	usleep(10);
 	return (1);
 }
-
-/*tatic void print_test(t_philo *philo)
-{
-	pthread_mutex_lock(philo->print_mutex);
-	long long int t;
-
-	t = get_timeval(philo->time.t_start);
-	printf("%lld %i\n", t / 1000, philo->id);
-	pthread_mutex_unlock(philo->print_mutex);
-}*/
 
 static int	ft_full(t_philo *philo)
 {
